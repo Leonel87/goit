2ab5c3b0d41791b6b6b0811f1dabca4c761148bd
Aleksey Uvarov <87alekseyuvarov@gmail.com>
2016-04-07 11:23:00 +0300

    dz4 tabs

--- Lesson 4js/js/main.js
+++ Lesson 4js/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
-    var currentAttrValue = $(this).attr('href');
-    $('.tabs' + currentAttrValue).show().siblings().hide();
-    $(this).parent('li').addClass('active').siblings().removeClass('active');
-    e.preventDefault();
+        var currentAttrValue = $(this).attr('href');
+        $('.tabs ' + currentAttrValue).show().siblings().hide();
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+         e.preventDefault();
     });
 });
