b02073964ae8822f899bd3c6fdfe5acd9cede626
Aleksey Uvarov <87alekseyuvarov@gmail.com>
2016-04-12 20:13:52 +0300

    dz6

--- Lesson 5js/js/script.js
+++ Lesson 5js/js/script.js
@@ -1,3 +1,45 @@
-$(function () {
-	$('.carousel').carousel();
-	});
+$(function() {
+    $('.jcarousel').jcarousel({
+        $('.jcarousel-prev').click(function() {
+    $('.jcarousel').jcarousel('scroll', '-=1');
+});
+
+$('.jcarousel-next').click(function() {
+    $('.jcarousel').jcarousel('scroll', '+=1');
+});
+    });
+});
+
+
+
+
+
+
+
+
+
+//плагин для слайда дз 11-12
+(function($){
+  $(function(){
+    $('.js-slider').mySlider({
+      dots: 'ololol',
+      arrows:true,
+      newKey:12315,
+    });
+  });
+})(jQuery);
+
+
+
+(function($){
+  $.fn.myslider = function(option) {
+    var _default = {
+      autoSlide: false,
+      dots:false,
+      arrows: false
+    }
+    var _setting = $.extend(_default, option);
+    var _this = $(_this);
+
+  };
+})(jQuery);
